a1e896bc2226cd35bf144ae99b8a945b792aa606
rarao <rahulrao.hsr@gmail.com>
2019-06-12 15:25:55 +0530

    Adding new project for sorting algos

---
+++ Sorting/SortingAlgorithm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Sorting
+{
+    public class SortingAlgorithm
+    {
+        public static void Swap(ref int a,ref int b)
+        {
+            int temp = a;
+            a=b;
+            b=temp;
+        }
+        public static void BubbleSort(int []arr)
+        {
+            int length = arr.Length;
+            bool swap = true;
+            for(int i=0;i<length-1 && swap;i++)
+            {
+                swap = false;
+                for(int j=0;j<length-1;j++)
+                {
+                    if(arr[j]>arr[j+1])
+                    {
+                        swap = true;
+                        Swap(ref arr[j],ref arr[j+1]);
+                    }
+                }
+            }
+        }
+    }
+}
